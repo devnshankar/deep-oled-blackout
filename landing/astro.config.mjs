9e858757b83c334e80e24ac6d5fb4af49c7c8a6e
devnshankar <subhashankarbehera7@gmail.com>
2025-07-01 09:27:00 +0530

    Fix: Fixed the jekyll issue

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  outDir: '../docs',
+  outDir: 'docs',
   base: '/deep-oled-blackout/', // this must match repo name if not using a custom domain
 });
